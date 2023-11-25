641552c4d961981e3906f49df3f9fda0d1e0f7be
Sam23656 <sem26m06@gmail.com>
2023-11-25 18:58:32 +0500

    Update StartPage

--- frontend/src/components/Start_Page/Footer.jsx
+++ frontend/src/components/Start_Page/Footer.jsx
@@ -2,8 +2,13 @@
 function Footer() {
 
     return(
-        <div>
-
+        <div className="flex flex-row justify-between items-center fixed bottom-0 bg-swirl-dark " style={{height: '60px', width: '100%'}}>
+            <p className="ms-3">Logo</p>
+            <div className="flex flex-col flex-wrap" style={{height: '50px', width: '80%'}}>
+            <a className="hover:underline" href="/Login">Login</a>
+            <a className="hover:underline" href="/Register">Register</a>
+            
+            </div>
         </div>
     )
 }
--- frontend/src/components/Start_Page/StartPage.jsx
+++ frontend/src/components/Start_Page/StartPage.jsx
@@ -3,13 +3,13 @@ import Footer from "./Footer";
 function StartPage() {
 
     return(
-        <>
+        <div className="flex flex-col">
             <Header />
-
-            
-
+            <div className="flex self-center mt-2 flex-wrap border-2 bg-swirl-dark rounded border-rust text-center items-center justify-center  flex-col" style={{height: '200px', width: '40%'}}>
+                <p>Start Page</p>
+            </div>
             <Footer />
-        </>
+        </div>
     )
 }
 
